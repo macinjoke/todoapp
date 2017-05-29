e1062cd7f4ae72575cb9815a0d3764787ed063d7
Shunji Makino <shunji.makino@gmail.com>
2017-05-30 00:46:30 +0900

    various changes

---
+++ server.py
@@ -0,0 +1,65 @@
+
+import json
+import os
+from flask import Flask, Response, request
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+
+
+@app.route('/load', methods=['GET'])
+def load():
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
+
+    return Response(
+        json.dumps(data),
+        mimetype='application/json',
+        headers={
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Origin': '*'
+        }
+    )
+
+
+@app.route('/move', methods=['POST'])
+def move():
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
+
+    body = request.data.decode('utf-8')
+    body_data = json.loads(body)
+
+    types = ['todo', 'done']
+    if body_data['type'] == types[0]:
+        from_key_name = '{}_list'.format(types[0])
+        to_key_name = '{}_list'.format(types[1])
+    elif body_data['type'] == types[1]:
+        from_key_name = '{}_list'.format(types[1])
+        to_key_name = '{}_list'.format(types[0])
+    else:
+        return Response(status=400)
+
+    for i, t in enumerate(data[from_key_name]):
+        if t['id'] == body_data['id']:
+            task = t
+            del data[from_key_name][i]
+            data[to_key_name].append(task)
+            break
+    else:
+        return Response(status=404)
+
+    with open('data.json', 'w') as f:
+        f.write(json.dumps(data, indent=2, ensure_ascii=False))
+
+    return Response(
+        json.dumps(data),
+        mimetype='application/json',
+        headers={
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Origin': '*'
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
--- src/App.js
+++ src/App.js
@@ -1,17 +1,146 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField'
 import logo from './logo.svg';
 import './App.css';
+
+injectTapEventPlugin()
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>TODO App</h2>
-        </div>
+        <Header />
+        <Content />
+        <Material />
       </div>
     );
+  }
+}
+
+const print = () => {
+  console.log('tekittooooo')
+}
+
+const Material = () => (
+  <MuiThemeProvider>
+    <div>
+      <RaisedButton label="Default" onTouchTap={print}/>
+      <TextField hintText="Hint Text" />
+    </div>
+  </MuiThemeProvider>
+)
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2 id="app-title">TODO App</h2>
+      </header>
+    )
+  }
+}
+
+class Content extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {data: ''};
+  }
+
+  loadCountFromServer = () => {
+    fetch('/load')
+      .then(res => {
+        if (!res.ok) {
+          throw Error(res.statusText)
+        }
+        return res.json()
+      })
+      .then(data => this.setState({data: data}));
+  }
+
+  moveTask = (body) => {
+    fetch('/move', {
+      method: 'POST',
+      body: JSON.stringify(body)
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw Error(res.statusText)
+        }
+        return res.json()
+      })
+      .then(data => this.setState({data: data}));
+  }
+
+  componentWillMount() {
+    this.loadCountFromServer()
+  }
+
+  componentDidMount() {
+    setInterval(this.loadCountFromServer, 10000)
+  }
+
+  render() {
+    return (
+      <div className="content" id="content">
+        <TaskList type="todo" taskList={this.state.data.todo_list}
+                  onMoveTask={this.moveTask}/>
+        <TaskList type="done" taskList={this.state.data.done_list}
+                  onMoveTask={this.moveTask}/>
+      </div>
+    )
+  }
+}
+
+class TaskList extends Component {
+  constructor(props){
+    super(props)
+    switch (props.type) {
+      case 'todo':
+        this.className = 'todoList'
+        this.title = 'TODO'
+        break
+      case 'done':
+        this.className = 'doneList'
+        this.title = 'DONE'
+        break
+      default:
+        break
+    }
+  }
+
+  render() {
+    let taskNodes = undefined
+    if (this.props.taskList !== undefined) {
+      taskNodes = this.props.taskList.map(task => {
+        const post_body = {
+          id: task.id,
+          type: this.props.type
+        }
+        return (
+          <li key={task.id}>
+            {task.title}
+            <button type="button" className="move-button"
+                    onClick={() => this.props.onMoveTask(post_body)}>
+              move
+            </button>
+          </li>
+        )
+      })
+    }
+    return (
+      <div className={this.className + " taskList"}>
+        <h2>{this.title}</h2>
+        <ul>
+          <li><button type="button" className="add-button">＋</button></li>
+          {taskNodes}
+          <li><button type="button" className="add-button">＋</button></li>
+        </ul>
+      </div>
+    )
   }
 }
 
